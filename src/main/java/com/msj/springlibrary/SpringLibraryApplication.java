506aab40459159971f290cc03634a2f711e12606
Mateus Souza <mateussouzaads@outlook.com>
2022-02-23 21:44:05 -0300

    refactor: add ModelMapper to the project

--- src/main/java/com/msj/springlibrary/SpringLibraryApplication.java
+++ src/main/java/com/msj/springlibrary/SpringLibraryApplication.java
@@ -1,10 +1,17 @@
 package com.msj.springlibrary;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringLibraryApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLibraryApplication.class, args);
--- src/main/java/com/msj/springlibrary/api/resource/BookController.java
+++ src/main/java/com/msj/springlibrary/api/resource/BookController.java
@@ -3,6 +3,7 @@ package com.msj.springlibrary.api.resource;
 import com.msj.springlibrary.api.dto.BookDTO;
 import com.msj.springlibrary.model.Book;
 import com.msj.springlibrary.service.BookService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,23 +15,17 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO createBook(@RequestBody BookDTO bookDTO) {
 
-        Book bookEntity = Book.builder()
-                .author(bookDTO.getAuthor())
-                .title(bookDTO.getTitle())
-                .isbn(bookDTO.getIsbn())
-                .build();
+        Book bookEntity = modelMapper.map(bookDTO, Book.class);
 
         bookEntity = bookService.save(bookEntity);
 
-        return BookDTO.builder()
-                .id(bookEntity.getId())
-                .author(bookEntity.getAuthor())
-                .title(bookEntity.getTitle())
-                .isbn(bookEntity.getIsbn())
-                .build();
+        return modelMapper.map(bookEntity, BookDTO.class);
     }
 }
